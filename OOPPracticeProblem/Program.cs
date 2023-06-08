8ec690649de34d3d6073a0d98c91121475cc5b85
Shrikant <sawantshrikant07@gmail.com>
2023-06-08 12:42:39 +0530

    [Shrikant] add:UniqueElementsInArray

--- OOPPracticeProblem/Program.cs
+++ OOPPracticeProblem/Program.cs
@@ -10,21 +10,35 @@ namespace OOPPracticeProblem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1. Count Duplicate Elements\n2. Exit\nEnter Your Option To Execute: ");
+                Console.WriteLine("1. Count Duplicate Elements\n2. Print Unique Elements\n3. Exit\nEnter Your Option To Execute: ");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Console.WriteLine("Enter the elements of the array (comma-separated):");
-                        string input = Console.ReadLine();
-                        int[] arr = Array.ConvertAll(input.Split(','), int.Parse);
+                        string input1 = Console.ReadLine();
+                        int[] arr1 = Array.ConvertAll(input1.Split(','), int.Parse);
 
                         DuplicateElements duplicateElements = new DuplicateElements();
-                        int duplicateCount = DuplicateElements.CountDuplicateElements(arr);
+                        int duplicateCount = DuplicateElements.CountDuplicateElements(arr1);
 
                         Console.WriteLine($"Total number of duplicate elements: {duplicateCount}");
                         break;
                     case 2:
+                        Console.WriteLine("Enter the elements of the array (comma-separated):");
+                        string input2 = Console.ReadLine();
+                        int[] arr2 = Array.ConvertAll(input2.Split(','), int.Parse);
+
+                        UniqueElements uniqueElements = new UniqueElements();
+                        int[] uniqueArr = uniqueElements.GetUniqueElements(arr2);
+
+                        Console.WriteLine("Unique elements in the array:");
+                        foreach (int num in uniqueArr)
+                        {
+                            Console.WriteLine(num);
+                        }
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
@@ -33,7 +47,5 @@ namespace OOPPracticeProblem
                 }
             }
         }
-
-        
     }
 }
---
+++ OOPPracticeProblem/UniqueElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticeProblem
+{
+    public class UniqueElements
+    {
+        public int[] GetUniqueElements(int[] arr)
+        {
+            Dictionary<int, int> elementCount = new Dictionary<int, int>();
+            foreach (int num in arr)
+            {
+                if (elementCount.ContainsKey(num))
+                    elementCount[num]++;
+                else
+                    elementCount[num] = 1;
+            }
+            List<int> uniqueList = new List<int>();
+           foreach(KeyValuePair<int, int> kvp in elementCount)
+           {
+                if (kvp.Value == 1) uniqueList.Add(kvp.Key);
+           }
+           return uniqueList.ToArray();
+        }
+    }
+}
