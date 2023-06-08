eda1a437c92ca26bb69d06ba6a285543e1964420
Shrikant <sawantshrikant07@gmail.com>
2023-06-08 12:16:04 +0530

    [Shrikant] add:CountDuplicateElementsInArray

---
+++ OOPPracticeProblem/DuplicateElements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OOPPracticeProblem
+{
+    public class DuplicateElements
+    {
+        public static int CountDuplicateElements(int [] arr)
+        {
+            Dictionary<int, int> elementCount = new Dictionary<int, int> ();
+            foreach (int num in arr)
+            {
+                if(elementCount.ContainsKey(num))
+                    elementCount[num]++;
+                else
+                    elementCount[num] = 1;
+            }
+            int duplicateCount = elementCount.Count(X => X.Value > 1);
+            return duplicateCount;  
+        }
+    }
+}
--- OOPPracticeProblem/Program.cs
+++ OOPPracticeProblem/Program.cs
@@ -1,7 +1,39 @@
-﻿public class Program
+﻿using System;
+
+namespace OOPPracticeProblem
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Welcome To Object-Oriented Programming Practice Problem!");
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Object-Oriented Programming Practice Problem!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. Count Duplicate Elements\n2. Exit\nEnter Your Option To Execute: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the elements of the array (comma-separated):");
+                        string input = Console.ReadLine();
+                        int[] arr = Array.ConvertAll(input.Split(','), int.Parse);
+
+                        DuplicateElements duplicateElements = new DuplicateElements();
+                        int duplicateCount = DuplicateElements.CountDuplicateElements(arr);
+
+                        Console.WriteLine($"Total number of duplicate elements: {duplicateCount}");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+
+        
     }
 }
